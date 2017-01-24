887c63b62ef57233802746c2d450a693b1766b22
Matt Tanguay-Carel <matthieutc@gmail.com>
2017-01-24 20:47:12 +0100

    creates basic app

---
+++ index.js
@@ -0,0 +1,7 @@
+import moment from 'moment';
+
+let fn = () => {
+  console.log("Hello from function")
+}
+
+fn();
---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const webpack = require('webpack')
+
+module.exports = {
+  entry: './index.js',
+  output: {
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015']
+        }
+      }
+    ]
+  }
+  // plugins: []
+}
